cf46f72e18e1a114bbfd1fffc0352c26bf9157f0
Bijan Farahani <bijancfarahani@gmail.com>
2024-12-07 03:29:22 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,51 @@
+use std::collections::HashSet;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process::ExitCode;
-use std::collections::HashSet;
 // Type inference lets us omit an explicit type signature (which
 // would be `HashSet<String>` in this example).
 use anyhow::Error;
 
-fn process_input(input: &String) -> Option<ExitCode>{
+fn process_input(input: &String) -> Option<ExitCode> {
     let mut commands = HashSet::new();
     commands.insert("exit");
     commands.insert("echo");
+    commands.insert("type");
 
     let tokens_orwhat: Vec<&str> = input.split_whitespace().collect();
     let command = tokens_orwhat[0];
 
-    if command == "type"
-    {
-        if tokens_orwhat.len() == 1
-        {
+    if command == "type" {
+        if tokens_orwhat.len() == 1 {
             println!("$ ");
+        } else if commands.contains(tokens_orwhat[1]) {
+            println!("{} is a shell builtin", tokens_orwhat[1]);
+        } else {
+            println!("{}: not found", tokens_orwhat[1]);
         }
-        if commands.contains(tokens_orwhat[1])
-        {
-            println!("{} is a shell builtin",tokens_orwhat[1]);
-        }
-        else {
-            println!("{}: not found",tokens_orwhat[1]);
-        }
+        None
     }
-    match command
-    {
+    match command {
         "exit" => Some(ExitCode::from(0)),
 
-        "echo" =>
-        {
-            for argument in tokens_orwhat.iter().skip(1)
-            {
+        "echo" => {
+            for argument in tokens_orwhat.iter().skip(1) {
                 print!("{}", argument);
-                if argument != tokens_orwhat.last().unwrap()
-                {
+                if argument != tokens_orwhat.last().unwrap() {
                     print!(" ");
                 }
             }
             println!("");
             None
         }
-        _ =>
-        {
+        _ => {
             println!("{}: command not found", command);
             None
         }
     }
 }
 
-fn repl_loop() -> Option<ExitCode>{
+fn repl_loop() -> Option<ExitCode> {
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -64,12 +56,10 @@ fn repl_loop() -> Option<ExitCode>{
     return process_input(&input);
 }
 
-fn main()  -> Result<ExitCode, &'static str> {
-    while true
-    {
+fn main() -> Result<ExitCode, &'static str> {
+    while true {
         let result = repl_loop();
-        if result.is_some()
-        {
+        if result.is_some() {
             return Ok(result.unwrap());
         }
     }
