8f0a03c2ea07d5d5ebed8f6c58153268de2cc2e0
Bijan Farahani <bijancfarahani@gmail.com>
2024-12-07 02:33:27 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,6 @@ fn main() {
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+    read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
 }
