14f5399b0b6f5a8e2fca784f6aba5c64bdbea52f
Bijan Farahani <bijancfarahani@gmail.com>
2024-12-07 03:03:57 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::ExitCode;
 
-fn repl_loop(){
+use anyhow::Error;
+
+fn process_input(input: &String) -> Option<ExitCode>{
+    let tokens_orwhat: Vec<&str> = input.split_whitespace().collect();
+    let command = tokens_orwhat[0];
+    match command
+    {
+        "exit" => Some(ExitCode::from(0)),
+
+        _ =>  None
+    }
+}
+
+fn repl_loop() -> Option<ExitCode>{
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -9,12 +23,17 @@ fn repl_loop(){
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
-    println!("{}: command not found", input.trim());
+    return process_input(&input);
 }
 
-fn main() {
+fn main()  -> Result<ExitCode, &'static str> {
     while true
     {
-        repl_loop();
+        let result = repl_loop();
+        if result.is_some()
+        {
+            return Ok(result.unwrap());
+        }
     }
+    Err("Past infinite loop")
 }
